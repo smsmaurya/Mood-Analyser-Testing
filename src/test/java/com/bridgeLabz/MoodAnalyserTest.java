457971e087aaec95196b9bf800d7593dd26844be
smsmaurya <sumeshmaurya786@gmail.com>
2023-11-06 06:03:50 +0530

    Welcome message

---
+++ src/test/java/com/bridgeLabz/MoodAnalyserTest.java
@@ -0,0 +1,11 @@
+package com.bridgeLabz;
+
+import org.junit.jupiter.api.Test;
+
+public class MoodAnalyserTest {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome in mood analyser test.");
+    }
+
+}
