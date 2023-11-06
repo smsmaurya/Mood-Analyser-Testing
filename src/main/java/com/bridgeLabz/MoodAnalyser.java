3bb663387fd1a336ab5025cb4320373d3a031f8b
smsmaurya <sumeshmaurya786@gmail.com>
2023-11-06 11:54:50 +0530

    Ability to analyse mood and respond happy or sad using constructor.

--- src/main/java/com/bridgeLabz/MoodAnalyser.java
+++ src/main/java/com/bridgeLabz/MoodAnalyser.java
@@ -1,7 +1,16 @@
 package com.bridgeLabz;
 
 public class MoodAnalyser {
-    
+
+    String message;
+    public MoodAnalyser(String message) {
+        this.message = message;
+    }
+
+    public MoodAnalyser() {
+
+    }
+
     public String moodAnalyse(String message) {
         if (message.contains("My mood is sad")){
             return "SAD";
@@ -9,6 +18,15 @@ public class MoodAnalyser {
         if (message.contains("My mood is happy")){
             return "HAPPY";
         }
-        return null;
+        return "";
+    }
+
+    public String moodAnalyserConstructor() {
+        if(message.contains("I am in sad mood")){
+            return "SAD";
+        } else if(message.contains("I am in Happy mood")){
+            return "HAPPY";
+        }
+        return "";
     }
 }
--- src/test/java/com/bridgeLabz/MoodAnalyserTest.java
+++ src/test/java/com/bridgeLabz/MoodAnalyserTest.java
@@ -9,14 +9,28 @@ public class MoodAnalyserTest {
 
     @Test
     public void givenMoodShouldReturnSad(){
-        String mood=moodAnalyser.moodAnalyse("My mood is sad");
+        String mood=moodAnalyser.moodAnalyse("I am in sad mood");
         Assertions.assertEquals("SAD",mood);
     }
 
     @Test
     public void givenMoodShouldReturnHappy(){
-        String mood = moodAnalyser.moodAnalyse("My mood is happy");
+        String mood = moodAnalyser.moodAnalyse("I am in happy mood");
         Assertions.assertEquals("HAPPY",mood);
     }
+
+    @Test
+    public void givenMoodShouldReturnSadUsingConstructor(){
+        MoodAnalyser moodAnalyser1 = new MoodAnalyser("I am in sad mood");
+        Assertions.assertEquals("SAD",moodAnalyser1.moodAnalyserConstructor());
+    }
+
+    @Test
+    public void givenMoodShouldReturnHappyUsingConstructor(){
+        MoodAnalyser moodAnalyser2 = new MoodAnalyser("I am in Happy mood");
+        Assertions.assertEquals("HAPPY",moodAnalyser2.moodAnalyserConstructor());
+    }
+
+
 
 }
